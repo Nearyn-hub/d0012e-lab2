8788ff1a46e1356a79844a178fad059474edddd1
hannes f <hannes.furhoff@hotmail.com>
2021-12-06 12:18:32 +0100

    del1 impl klar

--- del1.py
+++ del1.py
@@ -27,10 +27,19 @@ def smallest(a):
 
 
 
+def sortSublist(a):
+    if a[1] < a[0]:                 # c1: O(6)
+        a[0], a[1] = a[1], a[0]     
+    if a[2] < a[1]:                 
+        a[1], a[2] = a[2], a[1]     
+        if a[1] < a[0]:             
+            a[0], a[1] = a[1], a[0]
+    return a
+
 def merge(l, r):
-    ret = []                            # O(1)
-    while len(l) > 0 and len(r) > 0:    # 
-        if l[0] > r[0] or l[0] == r[0]: # 
+    ret = []                            
+    while len(l) > 0 and len(r) > 0:    
+        if l[0] > r[0] or l[0] == r[0]: 
             ret.append(r[0])
             del r[0]
         elif r[0] > l[0]:
@@ -42,30 +51,18 @@ def merge(l, r):
         ret.append(er)
     return ret
 
-def correct(a):
-    if len(a) >= 3: return list(smallest(a))    # c2 = O(4)
-    if len(a) == 1: return [a[0]]               
-    if len(a) == 2:
-        if a[0] > a[1]: return [a[0], a[1]]
-        else: return [a[1], a[0]]        
+def smallestDAC(a):
+    if len(a) == 3:
+        return sortSublist(a)
+    mid = len(a) // 2
+    l = smallestDAC(a[mid:])
+    r = smallestDAC(a[:mid])
+    ret = merge(l, r)[0:3]
+    return ret
 
-def smallestDAC(arr):
-    n = len(arr)                    # c1: O(4)
-    arr1 = arr[0      : n//3]
-    arr2 = arr[n//3   : 2*n//3]
-    arr3 = arr[2*n//3 : 3*n//3]
-
-    v1 = correct(arr1)              # c2: se def.
-    v2 = correct(arr2)
-    v3 = correct(arr3)
-
-    l1 = merge(v1, v2)              # c3: se def.
-    l2 = merge(l1, v3)
-
-    return (l2[0], l2[1], l2[2])    # O(1)
-
-# O(f(n))  =  c1 + 3(c2) + 2(c3) + c4  =  
-
-print(smallestDAC([1,2,3]))
-arr = [27, 5, 13, 28, 25, 48, 54, 56, 64, 64, 62, 2, 1, 3]
+print(smallestDAC([2, 3, 1]))
+arr = [27, 5, 13,
+       28, 25, 48,
+       54, 56, 64,
+       64, 62, 1]
 print(smallestDAC(arr))
--- del2.py
+++ del2.py
@@ -1,18 +1,37 @@
-def maxSubArraySum(a,size):
-      
-    max_so_far = a[0]
-    max_ending_here = 0
-      
-    for i in range(0, size):
-        max_ending_here = max_ending_here + a[i]
-        if (max_so_far < max_ending_here):
-            max_so_far = max_ending_here
- 
-        if max_ending_here < 0:
-            max_ending_here = 0  
-    return max_so_far
-  
+# solution adapted from  https://personal.utdallas.edu/~daescu/maxsa.pdf
+
+class Sum:
+    def __init__(self, cs, ms, msfx, mpfx):
+        currentSum = cs
+        maxSum = ms
+        maxSuffix = msfx
+        maxPrefix = mpfx
+        
+    totalSum = 0    #
+    maxSum = 0      #
+    maxSuffix = 0   #
+    maxPrefix = 0   #
+    
+
+def maxSubArray(a, low, high):
+    # base case; "list" is length 1, all sums are the current element
+    if l==h: 
+        return Sum(a[low], a[low], a[low], a[low])
+
+    # recursion step
+    mid = (low + high) // 2
+    leftChild = maxSubArray(a, low, mid)
+    rightChild = maxSubArray(a, mid + 1, high)
+
+    # compare the results of previous sum steps
+    nextSum = Sum()
+    nextSum.
+    return nextSum
+    
+    
+    
+    
 # Driver function to check the above function
 a  = [-13, -3, -25, -20, -3, -16, -23, -12, -5, -22, -15, -4, 7]
-print ("Maximum contiguous sum is", maxSubArraySum(a,len(a)))
+print ("Maximum contiguous sum is", maxSubArray(a, 0, len(a)-1))
 
